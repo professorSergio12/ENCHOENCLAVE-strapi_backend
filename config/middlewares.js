93c99e1dd7df600a56d32cd85f35d544abe85373
Rohit Rawat <107769619+professorSergio12@users.noreply.github.com>
2024-04-22 22:37:18 +0530

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,3 +10,15 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://enchoenclave-strapi-backend.onrender.com ', 'http://localhost:1337'],
+    },
+  },
+  // Add other middleware entries as needed...
+];
